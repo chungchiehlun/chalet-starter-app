a4f51d1c92004678871d25f5d940cb3a38d23f07
chungchiehlun <wuceh14678@gmail.com>
2017-11-01 21:36:57 +0800

    enhance asyncRoute hoc

--- template/chalet/src/routes/Counter/index.js
+++ template/chalet/src/routes/Counter/index.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import { injectReducer } from '../../store/reducers';
 
-function async(importComponent, importReducer, store) {
+function asyncRoute(importComponent, importReducer, store) {
   return class extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
         component: null,
-        reducer: null,
       };
     }
+    static displayName = 'asyncRoute(Counter)';
     async componentDidMount() {
       const { default: component } = await importComponent();
       const { default: reducer } = await importReducer();
+
+      /* Inject 'counter' reducer to store */
+      injectReducer(store, { key: 'counter', reducer });
+
       this.setState({
         component,
-        reducer,
       });
     }
     render() {
-      const { reducer, component } = this.state;
-      if (reducer) {
-        /*  Add the reducer to the store on key 'counter'  */
-        injectReducer(store, { key: 'counter', reducer });
-      }
+      const { component } = this.state;
+
+      // You could render a loading spinner instead of rendering null.
       return component ? React.createElement(component, this.props) : null;
     }
   };
 }
 
 export default store =>
-  async(
+  asyncRoute(
     /*
     - import() calls use promises internally. If using it with older browers, remember
     to shim Promise using a polyfill.
